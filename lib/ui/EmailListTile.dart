dcea8ee0f875201e62eb20c73a7b4477a2484e03
ones-and-zeroes <shreyansh.yadav.cse19@itbhu.ac.in>
2020-05-31 20:21:18 +0530

    all except sqlite part done

--- lib/ui/EmailListTile.dart
+++ lib/ui/EmailListTile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gmail17/data/EmailData.dart';
 import 'package:gmail17/ui/DetailPage.dart';
 import 'package:gmail17/ui/HomePage.dart';
+import 'package:provider/provider.dart';
 
 class EmailListTile extends StatelessWidget {
   final EmailItem tileEmailData;
@@ -14,44 +16,62 @@ class EmailListTile extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 0),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DetailPage(
-                toggleFavCallback: toggleFavCallback,
-                index: index,
-                data: tileEmailData,
-              ),
-            ),
-          );
-        },
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Text(tileEmailData.avatar),
+    return Dismissible(
+      background: Container(
+        color: Colors.redAccent,
+      ),
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        Provider.of<EmailData>(context, listen: false)
+            .deleteEmail(tileEmailData);
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Deleted"),
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Colors.redAccent,
+            duration: Duration(seconds: 3),
           ),
-          title: Text(tileEmailData.sender),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(tileEmailData.subject),
-              Text(tileEmailData.description),
-            ],
-          ),
-          trailing: Column(
-            children: <Widget>[
-              Text("${DateTime.now().hour}:${DateTime.now().minute}"),
-              Expanded(
-                child: IconButton(
-                  onPressed: toggleFavCallback,
-                  icon:
-                      Icon(tileEmailData.fav ? Icons.star : Icons.star_border),
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailPage(
+                  toggleFavCallback: toggleFavCallback,
+                  index: index,
+                  data: tileEmailData,
                 ),
-              )
-            ],
+              ),
+            );
+          },
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Text(tileEmailData.avatar),
+            ),
+            title: Text(tileEmailData.sender),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(tileEmailData.subject),
+                Text(tileEmailData.description),
+              ],
+            ),
+            trailing: Column(
+              children: <Widget>[
+                Text("${DateTime.now().hour}:${DateTime.now().minute}"),
+                Expanded(
+                  child: IconButton(
+                    onPressed: toggleFavCallback,
+                    icon: Icon(
+                        tileEmailData.fav ? Icons.star : Icons.star_border),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
